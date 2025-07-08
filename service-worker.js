b428ab1789edd82ed826cf71f43751debccc7ce5
viveklandge2006 <viveklandge2006@gmail.com>
2025-07-08 11:29:16 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'portfolio-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
